a0c9e560a82185fc5127846ab60d29e73158cbf0
이승우 <127829709+ls-rain@users.noreply.github.com>
2024-06-12 16:13:12 +0900

    [Platinum IV] Title: 도시 왕복하기 1, Time: 12 ms, Memory: 3276 KB -BaekjoonHub

--- 백준/Platinum/17412. 도시 왕복하기 1/도시 왕복하기 1.cc
+++ 백준/Platinum/17412. 도시 왕복하기 1/도시 왕복하기 1.cc
@@ -1,33 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <string.h>
 using namespace std;
 
-int n, p, cap[401][401], flow[401][401];
+int cap[401][401], flow[401][401];
+int n, p, res;
 
-int edmonds(int s, int e){
-	memset(flow, 0, sizeof(flow));
-	int res = 0;
-	while(true){
+int edmonds(){
+	while(1){
 		vector<int> path(n+1, -1);
 		queue<int> q;
-		q.push(s);
+		q.push(1);
 		while(!q.empty()){
 			int cur = q.front();
 			q.pop();
-			for(int nxt = 1; nxt<=n; nxt++){
-				if(cap[cur][nxt]-flow[cur][nxt]>0 && path[nxt]==-1){
-					q.push(nxt);
-					path[nxt] = cur;
-					if(nxt == e) break;
+			for(int i = 1; i <= n; i++){
+				if(cap[cur][i] - flow[cur][i] >0 && path[i] == -1){
+					path[i] = cur;
+					q.push(i);
 				}
 			}
 		}
-		if(path[e] == -1) break;
+		if(path[2] == -1) break;
 		int remain = 987654321;
-		for(int p = e; p != s; p = path[p]) remain = min(remain, cap[path[p]][p]-flow[path[p]][p]);
-		for(int p = e; p != s; p = path[p]){
+		for(int p = 2; p != 1; p = path[p]) remain = min(remain, cap[path[p]][p]- flow[path[p]][p]);
+		for(int p = 2; p != 1; p = path[p]){
 			flow[path[p]][p] += remain;
 			flow[p][path[p]] -= remain;
 		}
@@ -36,13 +33,13 @@ int edmonds(int s, int e){
 	return res;
 }
 
-
 int main(){
 	cin >> n >> p;
-	for(int i = 0; i<p; i++){
+	for(int i = 0; i < p; i++){
 		int v1, v2;
 		cin >> v1 >> v2;
 		cap[v1][v2] = 1;
+		cap[v2][v2] = 1;
 	}
-	cout << edmonds(1, 2);
+	cout << edmonds();
 }
