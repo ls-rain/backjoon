c05173bb4e1df4600f7627c4e61820126a3cae9b
이승우 <127829709+ls-rain@users.noreply.github.com>
2024-10-03 20:56:48 +0900

    [level 3] Title: 입국심사, Time: 53.09 ms, Memory: 7.05 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,25 +1,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+typedef long long ll;
 using namespace std;
+long long answer = 0;
+
+bool judge(ll mid, int n , vector<int> times){
+    ll cnt = 0;
+    for(int i = 0; i < times.size(); i++){
+        cnt += mid / times[i];
+    }
+    return cnt < n;
+}
 
 long long solution(int n, vector<int> times) {
-    long long answer = 0;
     sort(times.begin(), times.end());
-    long long start = 1;
-    long long end = (long long)times[times.size() - 1] * n;
-    long long mid;
-    while(start <= end){
-        mid = (start + end) / 2;
-        long long cnt = 0;
-        for(int i = 0; i < times.size(); i++){
-            cnt += mid / times[i];
+    ll left = 1, right = (ll)times[times.size() - 1] * n;
+    while(left <= right){
+        ll mid = (left + right) / 2;
+        if(judge(mid, n, times)){
+            left = mid + 1;
         }
-        if(cnt < n) start = mid + 1;
         else{
-            if(mid <= end) answer = mid;
-            end = mid - 1;
+            if(mid <= right) answer = mid;
+            right = mid - 1;
         }
     }
+    
     return answer;
 }
