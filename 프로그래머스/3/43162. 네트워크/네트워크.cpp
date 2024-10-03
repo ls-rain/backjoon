a6b2bdff0aa09aa473192ae2f9bda359b0f1bbef
이승우 <127829709+ls-rain@users.noreply.github.com>
2024-10-03 17:10:30 +0900

    [level 3] Title: 네트워크, Time: 2.40 ms, Memory: 6.49 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -2,22 +2,24 @@
 #include <vector>
 
 using namespace std;
-int visited[201];
-void dfs(int now, int n, vector<vector<int>> computers){
-    visited[now] = 1;    
+
+int answer = 0;
+bool visited[201];
+
+void dfs(int idx, int n, vector<vector<int>> computers){
+    visited[idx] = 1;
     for(int i = 0; i < n; i++){
-        if(!visited[i] && computers[now][i]) dfs(i, n, computers);
+        if(!visited[i] && computers[idx][i]) dfs(i, n, computers);
     }
 }
 
-
 int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
     for(int i = 0; i < n; i++){
         if(!visited[i]){
-            dfs(i, n, computers);
+            dfs(i ,n, computers);
             answer++;
         }
     }
+    
     return answer;
 }
