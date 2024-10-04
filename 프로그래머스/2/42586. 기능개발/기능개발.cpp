74b1a4d06c542b575f04d591c1649b7819ef4d8b
이승우 <127829709+ls-rain@users.noreply.github.com>
2024-10-05 08:54:57 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
@@ -10,7 +11,7 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     while(!q.empty()){
         int cnt = 0;
         for(int i = 0; i < speeds.size(); i++) progresses[i] += speeds[i];
-        while(!q.empty() && progresses[q.front()] >= 100){
+        while(!q.empty()&&progresses[q.front()] >= 100){
             cnt++;
             q.pop();
         }
