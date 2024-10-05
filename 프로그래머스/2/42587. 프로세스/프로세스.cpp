f712bd6d758fe383777f5c40021d8f9a8e1ba6e2
이승우 <127829709+ls-rain@users.noreply.github.com>
2024-10-05 09:03:34 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,22 +1,21 @@
 #include <string>
 #include <vector>
 #include <queue>
-
 using namespace std;
-priority_queue<int> pq;
+
+
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    queue<pair<int, int>> q;
+    priority_queue<int> pq;
+    queue<pair<int,int>> q;
     for(int i = 0; i < priorities.size(); i++){
-        q.push({i, priorities[i]});
-    }
-    for(int i : priorities){
-        pq.push(i);
+        pq.push(priorities[i]);
+        q.push({priorities[i], i});
     }
     while(!q.empty()){
-        if(q.front().second == pq.top()){
+        if(q.front().first == pq.top()){
             answer++;
-            if(location == q.front().first) break;
+            if(location == q.front().second) break;
             q.pop();
             pq.pop();
         }
